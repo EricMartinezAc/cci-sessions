ea294da181f1c2a53457fef21ca675436aea007e
Arturo Martinez <gerencia@climatecontrolsing.com>
2025-01-05 02:25:24 -0500

    upp5

--- src/utils/mongodb.connection.ts
+++ src/utils/mongodb.connection.ts
@@ -5,7 +5,11 @@ const Conexiondb = async () => {
 
     await mongoose
       .connect(
-        `${process.env.MONGODB_URI}${process.env.MONGODB_URI_config}`
+        `${process.env.MONGODB_URI}${process.env.MONGODB_URI_config}`,
+        {
+          serverSelectionTimeoutMS: 10000, 
+          socketTimeoutMS: 45000,
+        }
       )
     console.log("Database is connected");
   } catch (error: any) { console.error("Connection error:", error) }
